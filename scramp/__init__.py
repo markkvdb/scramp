f1c97a83b84869f5207ebb6c60143df83a6758d0
markkvdb <github@mvanderbroek.com>
2023-10-19 23:22:06 +0200

    Set version explicitly

--- scramp/__init__.py
+++ scramp/__init__.py
@@ -1,17 +1,6 @@
-import sys
-
-if sys.version_info >= (3, 8):
-    from importlib import metadata
-else:
-    from importlib_metadata import metadata
-
-from scramp.core import (
-    ScramClient,
-    ScramException,
-    ScramMechanism,
-    make_channel_binding,
-)
+from scramp.core import (ScramClient, ScramException, ScramMechanism,
+                         make_channel_binding)
 
 __all__ = [ScramClient, ScramMechanism, ScramException, make_channel_binding]
 
-__version__ = metadata.version("scramp")
+__version__ = "1.4.5"
